b6d6ce8b3448963f7f01873c996b23f5d8511d9e
Qandeel <$GH_USERNAME@users.noreply.github.com>
2024-04-17 17:57:41 +0500

    updated

--- accounts/models.py
+++ accounts/models.py
@@ -14,11 +14,9 @@ class Account(MPTTModel):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
-    account_type = models.CharField(max_length=36, choices=Account_Types)
+    account_type = models.CharField(max_length=40, choices=Account_Types)
     currency = models.CharField(max_length=3, choices=Currency_Type)
     balance_must_be = models.CharField(max_length=6, choices=Balance_Nature)
-    # rate at which this tax applies: The field rate_at_which_this_tax_applies in an accounting system is typically used to store the percentage rate of a particular tax that applies to a transaction or an account. This could be for various taxes like sales tax, value-added tax (VAT), or any other applicable taxes. The rate is crucial for calculating the tax amount that needs to be applied to a transaction.
-    tax_rate = models.DecimalField(max_digits=5, decimal_places=2, help_text=_('The tax rate (in percentage %) applicable to the account'))
 
     def __str__(self):
         return self.name
@@ -35,11 +33,10 @@ class Transaction(models.Model):
     transaction_type = models.CharField(_("Transaction_Type"), max_length=50, choices=Transaction_Type.choices
     )
     # transaction_medium = models.CharField(_("Transaction_Medium"), max_length=20, choices=TransactionMedium.choices, null=True, blank=True)
+
+
+    def __str__(self) -> str:
+        return f'{self.date}-{self.description}'
     
     def __str__(self):
         return f"{self.transaction_type} of {self.amount} on {self.timestamp}"
-
-    def calculate_tax(self):
-        # This will calculate 5% of the transaction amount
-        tax_amount = (self.amount * self.account.tax_rate) / 100
-        return tax_amount
